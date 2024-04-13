2871d363cd770896c85d5b06bd61613eac381be1
dothihongkhanh <dothihongkhanh22@gmail.com>
2024-04-13 23:59:49 +0700

    add lis p1

--- app/Imports/ListeningQuestionsImport.php
+++ app/Imports/ListeningQuestionsImport.php
@@ -19,49 +19,41 @@ class ListeningQuestionsImport implements ToModel, WithHeadingRow
         $this->levelId = $levelId;
         $this->partId = $partId;
     }
-
     public function model(array $row)
     {
-
-
         $imageFiles = request()->file('image_upload');
-
-        if ($imageFiles) {
-            foreach ($imageFiles as $imageFile) {
-                $imagePath = $imageFile->store('listening/images', 'public');
-                $image = new Image();
-                $image->url_image = Storage::url($imagePath);
-                $image->save();
-            }
-        }
-
         $audioFiles = request()->file('audio_upload');
 
-        if ($audioFiles) {
-            foreach ($audioFiles as $audioFile) {
-                $audioPath = $audioFile->store('listening/audios', 'public');
-                $audio = new Audio();
-                $audio->url_audio = Storage::url($audioPath);
-                $audio->save();
+        if ($imageFiles !== null && $audioFiles !== null && count($imageFiles) > 0 && count($audioFiles) > 0) {
+            foreach ($imageFiles as $key => $imageFile) {
+                $imageName = $imageFile->getClientOriginalName();
+
+                if ($imageName === $row['image']) {
+                    $audioFile = $audioFiles[$key];
+
+                    $imagePath = $imageFile->store('listening/images', 'public');
+                    $audioPath = $audioFile->store('listening/audios', 'public');
+
+                    $image = Image::create(['url_image' => Storage::url($imagePath)]);
+                    $audio = Audio::create(['url_audio' => Storage::url($audioPath)]);
+
+                    $question = Question::create([
+                        'question_title' => $row['question_title'],
+                        'id_image' => $image->id,
+                        'id_audio' => $audio->id,
+                        'id_level' => $this->levelId,
+                        'id_part' => $this->partId,
+                    ]);
+
+                    for ($j = 1; $j <= 4; $j++) {
+                        Answer::create([
+                            'id_question' => $question->id,
+                            'answer_text' => $row['answer_' . $j],
+                            'is_correct' => ($row['correct_answer'] === "Chọn đáp án " . chr(64 + $j)), // Sử dụng mã ASCII để chuyển đổi A, B, C, D thành 1, 2, 3, 4
+                        ]);
+                    }
+                }
             }
-        }
-
-        // Lưu thông tin vào database
-
-        $question = new Question();
-        $question->question_title = $row['question_title'];
-        $question->id_image = $image->id;
-        $question->id_audio = $audio->id;
-        $question->id_level = $this->levelId;
-        $question->id_part = $this->partId;
-        $question->save();
-
-        for ($i = 1; $i <= 4; $i++) {
-            $answer = new Answer();
-            $answer->id_question = $question->id;
-            $answer->answer_text = $row['answer_' . $i];
-            $answer->is_correct = ($row['correct_answer'] === substr($answer->answer_text, 0, 1));
-            $answer->save();
         }
     }
 }
